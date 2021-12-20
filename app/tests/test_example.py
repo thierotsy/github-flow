48e8eef227e4d24743de75dfb504d9b665b361fd
Thierry RAZAFIMANDIMBY <manetservice@gmail.com>
2021-12-20 15:39:26 +0300

    add first app files

---
+++ app/tests/test_example.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+from unittest import TestCase
+import pylint
+
+class TestExample(TestCase):
+
+    def test_example(self):
+        self.assertIsNotNone(pylint)
